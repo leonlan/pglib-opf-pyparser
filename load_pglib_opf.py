4c9794a244f5313c3d348365bc6763ffdd793cd4
Leon Lan <leon.lanyidong@gmail.com>
2022-04-14 11:21:53 +0200

    Infer correct types from dataframe

--- load_pglib_opf.py
+++ load_pglib_opf.py
@@ -46,45 +46,6 @@ def load_pglib_opf(path):
     return [bus_data, gen_data, branch_data]
 
 
-# --------------------------------Utils------------------------------------
-
-
-def remove_empty(L):
-    return list(filter(None, L))
-
-
-def parse_bus_data(rows):
-    return pd.DataFrame(columns=rows[0], data=rows[1:])
-
-
-def parse_generator_data(generator_rows, generator_cost_rows):
-    # Some data instances contain generator types as a comment, i.e., ";NG"
-    # but this is not contained in the header.
-    has_gen_type = len(generator_rows[0]) + 1 == len(generator_rows[1])
-    headers = generator_rows[0] + (["generator_type"] if has_gen_type else [])
-
-    # We determine how many cost coefficients are given
-    # and create the corresponding headers
-    n = int(generator_cost_rows[1][3])
-    headers += generator_cost_rows[0][:4] + [f"c({n-1-i})" for i in range(n)]
-
-    # Concatenate the generator and generator cost rows
-    # but ignore the generator type from generator cost is it is there
-    data_rows = [
-        generator_rows[i]
-        + (generator_cost_rows[i][:-1] if has_gen_type else generator_cost_rows[i])
-        for i in range(1, len(generator_rows))
-    ]
-
-    return pd.DataFrame(columns=headers, data=data_rows, dtype=object)
-
-
-def parse_branch_data(rows):
-    df = pd.DataFrame(columns=rows[0], data=rows[1:])
-    df["line_idx"] = df.index
-    return df
-
-
 def to_networkx(bus_data, gen_data, branch_data):
     """
     Makes a networkx instance from the passed-in data.
@@ -111,6 +72,54 @@ def to_networkx(bus_data, gen_data, branch_data):
     nx.set_node_attributes(G, busdict)
 
     return G
+
+
+# --------------------------------Utils------------------------------------
+
+
+def remove_empty(L):
+    return list(filter(None, L))
+
+
+def parse_bus_data(rows):
+    return infer_types(pd.DataFrame(columns=rows[0], data=rows[1:]))
+
+
+def parse_generator_data(generator_rows, generator_cost_rows):
+    # Some data instances contain generator types as a comment, i.e., ";NG"
+    # but this is not contained in the header.
+    has_gen_type = len(generator_rows[0]) + 1 == len(generator_rows[1])
+    headers = generator_rows[0] + (["generator_type"] if has_gen_type else [])
+
+    # We determine how many cost coefficients are given
+    # and create the corresponding headers
+    n = int(generator_cost_rows[1][3])
+    headers += generator_cost_rows[0][:4] + [f"c({n-1-i})" for i in range(n)]
+
+    # Concatenate the generator and generator cost rows
+    # but ignore the generator type from generator cost is it is there
+    data_rows = [
+        generator_rows[i]
+        + (generator_cost_rows[i][:-1] if has_gen_type else generator_cost_rows[i])
+        for i in range(1, len(generator_rows))
+    ]
+
+    return infer_types(pd.DataFrame(columns=headers, data=data_rows, dtype=object))
+
+
+def parse_branch_data(rows):
+    df = pd.DataFrame(columns=rows[0], data=rows[1:])
+    df["line_idx"] = df.index
+    return infer_types(df)
+
+
+def infer_types(df):
+    """
+    # HACK: Infer the correct types from the dataframe. Currently, the provided
+    pandas methods (df.convert_types, df.infer_types) do not work. The workaround
+    is to convert to csv, then read as csv again.
+    """
+    return pd.read_csv(io.StringIO(df.to_csv(index=False)))
 
 
 bus_data, gen_data, branch_data = load_pglib_opf("pglib-opf/pglib_opf_case30_ieee.m")
